dada14f5fa78bcd7c8e6544dc280d2a82abe6e8d
Michael Gleason & Jordan Luyke <pair+fireflypulse.jluyke@devbootcamp.com>
2014-04-10 15:48:13 -0700

    bug fix with colons
    deleted newline

--- db/migrate/20140410190418_create_users.rb
+++ db/migrate/20140410190418_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :username
       t.string :password_digest
-      t.string :walk_speed, default => "normal"
+      t.string :walk_speed, default: "normal"
       t.string :phone_number
       t.string :email
       t.timestamps
