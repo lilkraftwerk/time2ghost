b242c77ebcde5069d3a46e1b8df9c2c9b1ca964f
Michael Gleason & Jordan Luyke <pair+fireflypulse.jluyke@devbootcamp.com>
2014-04-10 14:26:46 -0700

    added validations

--- app/models/User.rb
+++ app/models/User.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password
+  attr_accessible :username, :walk_speed, :phone_number, :email
+  validates :username, presence: :true, uniqueness: true
+  validates :phone_number, presence: true
+  validates :email, presence: true
+  validates :password, length: { in: 6..20 }
 end
