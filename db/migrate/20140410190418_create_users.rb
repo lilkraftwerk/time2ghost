8d955d6fce2474fca07e84cda2f98fdcb5d22f7e
Michael Gleason & Jordan Luyke <pair+fireflypulse.jluyke@devbootcamp.com>
2014-04-10 14:27:19 -0700

    added default walk speed

--- db/migrate/20140410190418_create_users.rb
+++ db/migrate/20140410190418_create_users.rb
@@ -3,9 +3,10 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :username
       t.string :password_digest
-      t.string :walk_speed
+      t.string :walk_speed, default => "normal"
       t.string :phone_number
       t.string :email
+      t.timestamps
     end
   end
 end
