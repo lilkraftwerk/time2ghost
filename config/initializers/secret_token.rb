56a69f296869d855cc74352009a933fb6ba166ac
Lucas Mathis <lucasquinnmathis@gmail.com>
2014-04-10 11:23:40 -0700

    let's get ghost

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Time2ghost::Application.config.secret_token = 'e9741c1043a863de5a777f55e6f65da4147e345f24f89d73dc6593ea08a924e2886d03cbd4f311edd5fe3d7b8b571c0c07b8460af76baa233147068319df0b9d'
