8364465b778f76347f60e311bb8867bc2416a6a2
Lucas Mathis <lucasquinnmathis@gmail.com>
2014-04-11 13:23:11 -0700

    testing bart XML parsing

--- app/models/bart.rb
+++ app/models/bart.rb
@@ -1,28 +1,39 @@
 class Bart
+  require 'open-uri'
+
   def self.get_departures(origin, destination)
-    route = self.get_correct_route(origin, destination)
-    endpoint = self.get_endpoint(route)
-    self.get_realtime_departures_on_route(origin, endpoint)
+    route_xml = self.get_route_xml(origin, destination)
+    route = parse_route_xml(route_xml)
+    endpoint_xml = self.get_endpoint_xml(route)
+    endpoint = self.parse_endpoint_xml(endpoint_xml)
+    realtime_xml = self.get_realtime_departure_xml(origin, endpoint)
+    self.parse_realtime_departure_xml(realtime_xml, endpoint)
   end
 
-  private
-
-  def self.get_correct_route(origin, destination)
-    raw_xml = open("http://api.bart.gov/api/sched.aspx?cmd=depart&orig=#{origin}&dest=#{destination}&key=MW9S-E7SL-26DU-VV8V") {|xml| xml.read }
-    Nokogiri::XML(raw_xml).at_xpath('//leg').attributes["line"].value
+  def self.get_route_xml(origin, destination)
+    open("http://api.bart.gov/api/sched.aspx?cmd=depart&orig=#{origin}&dest=#{destination}&key=MW9S-E7SL-26DU-VV8V") {|xml| xml.read }
   end
 
-  def self.get_endpoint(route_number)
-    endpoint = open("http://api.bart.gov/api/route.aspx?cmd=routeinfo&route=#{route_number[-1]}&key=MW9S-E7SL-26DU-VV8V") {|xml| xml.read }
-    Nokogiri::XML(endpoint).at_xpath('//destination').content
+  def self.parse_route_xml(route_xml)
+    Nokogiri::XML(route_xml).at_xpath('//leg').attributes["line"].value
   end
 
-  def self.get_realtime_departures_on_route(origin, endpoint)
-    puts "origin, endpoint: #{origin} / #{endpoint}"
-    arrival_times = []
-    realtime_xml = open("http://api.bart.gov/api/etd.aspx?cmd=etd&orig=#{origin}&key=MW9S-E7SL-26DU-VV8V") {|xml| xml.read }
+  def self.get_endpoint_xml(route_number)
+    open("http://api.bart.gov/api/route.aspx?cmd=routeinfo&route=#{route_number[-1]}&key=MW9S-E7SL-26DU-VV8V") {|xml| xml.read }
+  end
+
+  def self.parse_endpoint_xml(endpoint_xml)
+    Nokogiri::XML(endpoint_xml).at_xpath('//destination').content
+  end
+
+  def self.get_realtime_departure_xml(origin, endpoint)
+    open("http://api.bart.gov/api/etd.aspx?cmd=etd&orig=#{origin}&key=MW9S-E7SL-26DU-VV8V") {|xml| xml.read }
+  end
+
+  def self.parse_realtime_departure_xml(realtime_xml, endpoint)
     path = Nokogiri::XML(realtime_xml).xpath('//etd')
     correct_destination = path.xpath("//abbreviation[contains(text(), '#{endpoint}')]").first.parent()
+    arrival_times = []
     correct_destination.search('minutes').each {|x| arrival_times << x.text}
     arrival_times
   end
