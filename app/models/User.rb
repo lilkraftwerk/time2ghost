8b545e5389c4b3a610ff8335b8a759dfe4d6c79f
Michael Gleason & Jordan Luyke <pair+fireflypulse.jluyke@devbootcamp.com>
2014-04-11 13:05:23 -0700

    changed max pass length to 50 due to faker gem

--- app/models/User.rb
+++ app/models/User.rb
@@ -4,5 +4,5 @@ class User < ActiveRecord::Base
   validates :username, presence: :true, uniqueness: true
   validates :phone_number, presence: true
   validates :email, presence: true
-  validates :password, length: { in: 6..30 }
+  validates :password, length: { in: 6..50 }
 end
