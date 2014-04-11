8048dd0f2af183d17dadb956bea93e41b52329b8
Lucas Mathis <lucasquinnmathis@gmail.com>
2014-04-11 12:06:55 -0700

    BART api access working with nokogiri
    * enter origin, destination and receive realtime departure info

--- app/models/bart.rb
+++ app/models/bart.rb
@@ -1,42 +1,29 @@
 class Bart
   def self.get_departures(origin, destination)
-    route_number = self.get_correct_route(origin, destination)
-    endpoint = self.get_endpoint_of_route(route_number[-1])
-    realtime_departures = self.get_realtime_departures_on_route(origin, endpoint)
+    route = self.get_correct_route(origin, destination)
+    endpoint = self.get_endpoint(route)
+    self.get_realtime_departures_on_route(origin, endpoint)
   end
-  # gets the numbered BART route
-  # between two stations
-  # (ex: MLBR, POWL)
+
+  private
 
   def self.get_correct_route(origin, destination)
-    our_route = HTTParty.get("http://api.bart.gov/api/sched.aspx?cmd=depart&orig=#{origin}&dest=#{destination}&key=MW9S-E7SL-26DU-VV8V")
-    # ap our_route
-    our_route = our_route["root"]["schedule"]["request"]["trip"].first["leg"]
-    self.check_route(our_route)
+    raw_xml = open("http://api.bart.gov/api/sched.aspx?cmd=depart&orig=#{origin}&dest=#{destination}&key=MW9S-E7SL-26DU-VV8V") {|xml| xml.read }
+    Nokogiri::XML(raw_xml).at_xpath('//leg').attributes["line"].value
   end
 
-  def self.check_route(our_route)
-    if our_route.length > 1
-      final_route = our_route.first["line"]
-    else
-      final_route = our_route["line"]
-    end
-    final_route
+  def self.get_endpoint(route_number)
+    endpoint = open("http://api.bart.gov/api/route.aspx?cmd=routeinfo&route=#{route_number[-1]}&key=MW9S-E7SL-26DU-VV8V") {|xml| xml.read }
+    Nokogiri::XML(endpoint).at_xpath('//destination').content
   end
-  # gets the endpoint of a route by number
-
-  def self.get_endpoint_of_route(route_number)
-    endpoint = HTTParty.get("http://api.bart.gov/api/route.aspx?cmd=routeinfo&route=#{route_number}&key=MW9S-E7SL-26DU-VV8V")
-    endpoint["root"]["routes"]["route"]["config"]["station"].last
-  end
-
-  # gets the three closest departures from your origin station on the above route
 
   def self.get_realtime_departures_on_route(origin, endpoint)
+    puts "origin, endpoint: #{origin} / #{endpoint}"
     arrival_times = []
-    request = HTTParty.get("http://api.bart.gov/api/etd.aspx?cmd=etd&orig=#{origin}&key=MW9S-E7SL-26DU-VV8V")
-    lines = request["root"]["station"]["etd"]
-    lines["estimate"].each {|arrival| arrival_times << arrival["minutes"]}
+    realtime_xml = open("http://api.bart.gov/api/etd.aspx?cmd=etd&orig=#{origin}&key=MW9S-E7SL-26DU-VV8V") {|xml| xml.read }
+    path = Nokogiri::XML(realtime_xml).xpath('//etd')
+    correct_destination = path.xpath("//abbreviation[contains(text(), '#{endpoint}')]").first.parent()
+    correct_destination.search('minutes').each {|x| arrival_times << x.text}
     arrival_times
   end
 end
