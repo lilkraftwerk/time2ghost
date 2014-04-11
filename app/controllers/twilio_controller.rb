303333c074af079d919d7eb7c37deb3b8e012595
lilkraftwerk <lucasquinnmathis@gmail.com>
2014-04-10 19:46:09 -0700

    twilio working, bart updated slightly
    * twilio integration via model
    * bart code works, but is spaghetti as heck

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -1,11 +1,15 @@
 class TwilioController < ApplicationController
   def show
+      first = ["RICH", "LAKE", "GLEN", "POWL", "FRMT", "MLBR", "LAFY", "DUBL", "CIVC", "BAYF", "COLS", "CAST", "SBRN"].sample
+      second = ["RICH", "LAKE", "GLEN", "POWL", "FRMT", "MLBR", "LAFY", "DUBL", "CIVC", "BAYF", "COLS", "CAST", "SBRN"].sample
 
+      bart = Bart.get_departures(first, second)
+      bart.map!{|x| x + " minutes. "}
       @client = Twilio::REST::Client.new ENV['TWILIO_SID'], ENV['TWILIO_AUTH']
       @client.account.messages.create(
        :from => '+14155211220',
        :to => '+18184212905',
-       :body => 'swag me out'
+       :body => "times from #{first} to #{second}: #{bart}"
       )
   end
 end
--- app/models/bart.rb
+++ app/models/bart.rb
@@ -1,3 +1,4 @@
+
 class Bart
   def self.get_departures(origin, destination)
     route_number = self.get_correct_route(origin, destination)
@@ -10,13 +11,12 @@ class Bart
 
   def self.get_correct_route(origin, destination)
     our_route = HTTParty.get("http://api.bart.gov/api/sched.aspx?cmd=depart&orig=#{origin}&dest=#{destination}&key=MW9S-E7SL-26DU-VV8V")
-    # ap our_route
     our_route = our_route["root"]["schedule"]["request"]["trip"].first["leg"]
     self.check_route(our_route)
   end
 
   def self.check_route(our_route)
-    if our_route.length > 1
+    if our_route.is_a?(Array)
       final_route = our_route.first["line"]
     else
       final_route = our_route["line"]
@@ -36,7 +36,16 @@ class Bart
     arrival_times = []
     request = HTTParty.get("http://api.bart.gov/api/etd.aspx?cmd=etd&orig=#{origin}&key=MW9S-E7SL-26DU-VV8V")
     lines = request["root"]["station"]["etd"]
-    lines["estimate"].each {|arrival| arrival_times << arrival["minutes"]}
+    if lines.is_a?(Hash)
+      lines["estimate"].each {|arrival| arrival_times << arrival["minutes"]}
+    else
+      new_lines = lines.select {|current_line| current_line["abbreviation"] == endpoint}.first
+      if new_lines.nil?
+        lines.first["estimate"].each {|arrival| arrival_times << arrival["minutes"]}
+      else
+        new_lines["estimate"].each {|arrival| arrival_times << arrival["minutes"]}
+      end
+    end
     arrival_times
   end
 end
