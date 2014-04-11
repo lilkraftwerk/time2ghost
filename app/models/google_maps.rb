05a6f114836fc4700bd87712c7d49e9622205f77
Michael Gleason & Jordan Luyke <pair+fireflypulse.jluyke@devbootcamp.com>
2014-04-11 11:23:19 -0700

    renamed google maps model and refactored

---
+++ app/models/google_maps.rb
@@ -0,0 +1,39 @@
+class GoogleMaps
+  require 'uri'
+
+  def self.http_get_directions(origin, destination)
+    url = self.assemble_directions_request(origin, destination)
+
+    HTTParty.get(url)
+  end
+
+  def self.get_total_walking_time(json_object)
+    json_object["routes"][0]["legs"][0]["duration"]["text"]
+  end
+
+  def self.get_directions(json_object)
+    directions = []
+    json_object["routes"][0]["legs"][0]["steps"].each do |step|
+      directions << step["html_instructions"]
+    end
+
+    directions
+  end
+
+  private
+  def self.assemble_directions_request(origin, destination)
+    params = {
+      :origin => origin,
+      :destination => destination,
+      :sensor => false,
+      :mode => "walking"
+    }
+
+    "https://maps.googleapis.com/maps/api/directions/json?" + URI.encode_www_form(params)
+  end
+
+end
+
+
+# Demo link
+# https://maps.googleapis.com/maps/api/directions/json?origin=717+california+street&destination=montgomery+st+station&sensor=false&mode=walking
