60c096c0d44de8e9a2dc4b78ca1d6655ad3fd5e1
Michael Gleason <michaelgleason@Michaels-MacBook-Pro.local>
2014-04-10 19:43:21 -0700

    Google model now fetches directions and returns json object

--- app/models/google.rb
+++ app/models/google.rb
@@ -1,5 +1,16 @@
 class Google
 
+  def self.get_directions(origin, destination)
+    origin = origin.split(/[^\w-]+/).join("+")
+    destination = destination.split(/[^\w-]+/).join("+")
+    # Returns a json object
+    directions = HTTPary.get("https://maps.googleapis.com/maps/api/directions/json?origin=#{origin}&destination=#{destination}&sensor=false&mode=walking")
+    return directions
+  end
 
 
 end
+
+
+# Demo link
+# https://maps.googleapis.com/maps/api/directions/json?origin=717+california+street&destination=montgomery+st+station&sensor=false&mode=walking
