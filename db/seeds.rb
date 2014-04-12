3bc477a8dcca52f5aae65b028cd085df4e278c3a
lilkraftwerk <lucasquinnmathis@gmail.com>
2014-04-11 17:58:53 -0700

    seeded database with station information
    * gets all 44 stations, lat, long, all that

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'open-uri'
+require 'nokogiri'
+
+stations = open('http://api.bart.gov/api/stn.aspx?cmd=stns&key=MW9S-E7SL-26DU-VV8V') {|xml| xml.read}
+
+parsed_stations = Nokogiri::XML(stations).xpath('//station')
+
+parsed_stations.each do |station|
+ station_params = {
+    :abbr => station.search('abbr')[0].text,
+    :name => station.search('name')[0].text,
+    :latitude => station.search('gtfs_latitude')[0].text,
+    :longitude => station.search('gtfs_longitude')[0].text,
+    :address => station.search('address')[0].text,
+    :city => station.search('city')[0].text,
+    :state => station.search('state')[0].text,
+    :zipcode => station.search('zipcode')[0].text }
+    Station.create(station_params)
+
+end
