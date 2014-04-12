74942d5650cdbf06218b099d19e98298f86b71ea
Michael Gleason & Jordan Luyke <pair+fireflypulse.jluyke@devbootcamp.com>
2014-04-11 18:20:04 -0700

    Mass assignment and the beginnings of creating a trip.

--- app/models/Trip.rb
+++ app/models/Trip.rb
@@ -1,3 +1,6 @@
 class Trip < ActiveRecord::Base
+  attr_accessible :user_id, :departure_station, :destination_station, :walking_time, :directions, :departure_time, :bart_line
+  attr_accessor :current_location
+
 
 end
