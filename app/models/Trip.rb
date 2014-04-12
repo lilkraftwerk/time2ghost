e645859dca39a0dbb4243e04eb43a2337c54e474
Jordan Luyke & Lucas Mathis <pair+jluyke.lilkraftwerk@devbootcamp.com>
2014-04-12 13:00:21 -0700

    cleaned up update_departure_time by splitting up in methods

--- app/models/Trip.rb
+++ app/models/Trip.rb
@@ -1,29 +1,45 @@
 class Trip < ActiveRecord::Base
-  attr_accessible :user_id, :departure_station, :destination_station, :walking_time, :directions, :train_departing_time, :bart_line, :time_to_leave
+  attr_accessible :user_id, :departure_station, :destination_station, :walking_time, :directions, :train_departing_time, :bart_line, :recommended_leave_time
   attr_accessor :current_location
 
   def update_departure_time
     depart_station_obj = get_station(self.departure_station)
+
     self.walking_time = get_walking_time_to_station(current_location, depart_station_obj.gmap_destination_string)
 
+    potential_depart_times = get_depart_times_and_line
+
+    minutes_until_departure = get_minutes_until_train_departs(potential_depart_times)
+
+    set_train_departing_time(minutes_until_departure)
+
+    suggested_leave_time_in_minutes_from_now = minutes_until_departure - walking_time - 5
+
+    set_recommended_leave_time(suggested_leave_time_in_minutes_from_now)
+  end
+
+  private
+  def set_recommended_leave_time(suggested_leave_time_in_minutes_from_now)
+    self.recommended_leave_time = Time.now + suggested_leave_time_in_minutes_from_now.minutes
+  end
+
+  def get_depart_times_and_line
     depart_times = Bart.get_departures(self.departure_station, self.destination_station)
     self.bart_line = get_station(depart_times.pop).name
+    depart_times
+  end
 
-    depart_times.each do |depart_time|
-      self.time_to_leave = depart_time.to_i - walking_time
-      if time_to_leave >= 5
-        self.train_departing_time = Time.now + depart_time.to_i
-        break
-      end
-    end
+  def set_train_departing_time(minutes_until_departure)
+    self.train_departing_time = Time.now + minutes_until_departure.to_i.minutes
+  end
+
+  def get_minutes_until_train_departs(potential_depart_times)
+    potential_depart_times.find { |depart_time| depart_time.to_i - self.walking_time - 5 > 0 }.to_i
   end
 
   def get_station(abbr)
     Station.find_by_abbr(abbr.upcase)
   end
-
-  # def get_directions # not in mvp
-  # end
 
   def get_walking_time_to_station(origin, destination)
     gmaps_json = GoogleMaps.http_get_directions(origin, destination)
