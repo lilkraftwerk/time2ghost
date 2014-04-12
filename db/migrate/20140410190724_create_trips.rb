f5df14efb2a88d1d6ddc6a1f165a49d9f3b3cd68
Michael Gleason & Jordan Luyke <pair+fireflypulse.jluyke@devbootcamp.com>
2014-04-11 17:23:51 -0700

    Added directions column and renamed destination to make more sense

--- db/migrate/20140410190724_create_trips.rb
+++ db/migrate/20140410190724_create_trips.rb
@@ -3,8 +3,9 @@ class CreateTrips < ActiveRecord::Migration
     create_table :trips do |t|
       t.belongs_to :user
       t.string :departure_station
-      t.string :destination
+      t.string :destination_station
       t.time :walking_time
+      t.text :directions
       t.time :departure_time
       t.string :bart_line
       t.timestamps
