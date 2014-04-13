2998c92b8d34e72728999273723641e0773f26b0
& <pair+fireflypulse.jluyke@devbootcamp.com>
2014-04-13 15:36:08 -0700

    Converted to MVC.

--- app/assets/javascripts/geolocation.js
+++ app/assets/javascripts/geolocation.js
@@ -1,41 +1,69 @@
-form_addres = document.querySelectorAll('.demo')
+var geoLoc = (function() {
 
-function getLocation(){
-  if ("geolocation" in navigator){
-    navigator.geolocation.getCurrentPosition(onSuccess, showError)
-  }
-  else {
-    alert('Geolocation is not supported in your browser.')
-  }
-}
+  var init = function() {
+    var binder = new Binder();
+    binder.bind();
+  };
 
-function onSuccess(position){
-  var coords = position.coords.latitude + "," + position.coords.longitude;
-  $("#trip_current_location").val(coords);
-}
+  var Binder = function() {};
 
-function showError(error) {
-    switch(error.code) {
-        case error.PERMISSION_DENIED:
-            alert("User denied the request for Geolocation.");
-            break;
-        case error.POSITION_UNAVAILABLE:
-            alert("Location information is unavailable.");
-            break;
-        case error.TIMEOUT:
-            alert("The request to get user location timed out.");
-            break;
-        case error.UNKNOWN_ERROR:
-            alert("An unknown error occurred.");
-            break;
+  Binder.prototype = {
+    bind: function() {
+      var controller = new Controller();
+      this.bindGeolocate(controller);
+    },
+
+    bindGeolocate: function(controller) {
+      $('#geolocateButton').on('click', function(){
+        controller.getLocation();
+      });
     }
-}
+  }
+
+  var Controller = function() {};
+
+  Controller.prototype = {
+    getLocation: function(){
+      if ("geolocation" in navigator){
+        navigator.geolocation.getCurrentPosition(this.onSuccess, this.showError);
+      }
+      else {
+        alert('Geolocation is not supported in your browser.');
+      }
+    },
+
+    onSuccess: function(position){
+      var coords = position.coords.latitude + "," + position.coords.longitude;
+      $("#trip_current_location").val(coords);
+    },
+
+    showError: function(error) {
+      switch(error.code) {
+        case error.PERMISSION_DENIED:
+          alert("User denied the request for Geolocation.");
+          break;
+        case error.POSITION_UNAVAILABLE:
+          alert("Location information is unavailable.");
+          break;
+        case error.TIMEOUT:
+          alert("The request to get user location timed out.");
+          break;
+        case error.UNKNOWN_ERROR:
+          alert("An unknown error occurred.");
+          break;
+      }
+    }
+  };
+
+  return {
+    init: init
+  }
+
+})();
 
 $(document).ready(function(){
+  geoLoc.init();
 
-  $('#geolocateButton').on('click', function(){
-      getLocation();
-  })
 
 })
 
