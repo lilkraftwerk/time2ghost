23f3253e6f5db82ef18480e937cba9b21b839c01
Michael Gleason <mikeglea21@gmail.com>
2014-04-13 14:15:32 -0700

    Added Success and Error functions to geolocation
    * Success function just alerts user at the moment.
    * Error function pulls error out using switch case

--- app/assets/javascripts/geolocation.js
+++ app/assets/javascripts/geolocation.js
@@ -2,13 +2,41 @@ form_addres = document.querySelectorAll('.demo')
 
 function getLocation(){
   if ("geolocation" in navigator){
-    navigator.geolocation.getCurrentPosition(function(position){
-      x[0].innerHTML = "Your lat, long: " + position.coords.latitude + ", " + position.coords.longitude
+    navigator.geolocation.getCurrentPosition(onSuccess, showError){
     })
   }
   else {
     // User is going to have to input location themselves
+    alert('Geolocation is not supported in your browser.')
   }
 }
 
-getLocation()
+function onSuccess(position){
+  alert("Your lat, long: " + position.coords.latitude + ", " + position.coords.longitude)
+
+}
+
+function showError(error) {
+    switch(error.code) {
+        case error.PERMISSION_DENIED:
+            alert("User denied the request for Geolocation.");
+            break;
+        case error.POSITION_UNAVAILABLE:
+            alert("Location information is unavailable.");
+            break;
+        case error.TIMEOUT:
+            alert("The request to get user location timed out.");
+            break;
+        case error.UNKNOWN_ERROR:
+            alert("An unknown error occurred.");
+            break;
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function(){
+
+
+
+})
+
+// getLocation()
