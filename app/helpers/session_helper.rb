5744b79e2d8fa6e53dd69852cde0b31e5a8a89d6
Nicholas Cu <null>
2014-04-12 18:13:00 -0700

    made sessions work for logging out

---
+++ app/helpers/session_helper.rb
@@ -0,0 +1,9 @@
+module SessionHelper
+  def logged_in?
+    !!session[:user_id]
+  end
+
+  def correct_user
+    User.find(session[:user_id]) if logged_in?
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Time2ghost::Application.routes.draw do
-  root :to => 'homepage#index'
+  root :to => 'homepage#index', :constraints => true
+  # p session[:user_id]
   resources :trips
   resources :users
-  resources :sessions, :only => [:create, :delete]
+  resources :sessions, :only => [:create, :destroy]
   get 'twiliotest', to: 'twilio#show'
 end
