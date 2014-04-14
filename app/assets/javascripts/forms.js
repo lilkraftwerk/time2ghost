b28abf66b1f6058deeb1d6f9906c12b4eee47b0c
Lucas Mathis <lucasquinnmathis@gmail.com>
2014-04-13 18:11:43 -0700

    reverting js changes to working version

--- app/assets/javascripts/forms.js
+++ app/assets/javascripts/forms.js
@@ -1,25 +1,27 @@
 $( function() {
-      $("#user_submit").prop("disabled", true); // disable the submit button
+      $("#new_user[type='submit']").prop("disabled", true); // disable the submit button
       $("#user_phone_number").formance("format_phone_number") // setup the formatter
                              .on( 'keyup change blur', function (event) { // setup the event listeners to validate the field whenever the user takes an action
                                if ( $(this).formance('validate_phone_number') && ($('#user_email').formance('validate_email')))
-                                 $("#user_submit").prop("disabled", false); // enable the submit button if valid phone number
+                                 $("#new_user[type='submit']").prop("disabled", false); // enable the submit button if valid phone number
                                else
-                                 $("#user_submit").prop("disabled", true); // disable the submit button if invalid phone number
+                                 $("#new_user[type='submit']").prop("disabled", true); // disable the submit button if invalid phone number
                              });
 
       $("#user_email").formance("format_email")
                             .on( 'keyup change blur', function (event) {
                               if ( $(this).formance('validate_email') && $('#user_phone_number').formance('validate_phone_number') )
-                                  $("#user_submit").prop("disabled", false); // enable the submit button if valid phone number
+                                  $("#new_user[type='submit']").prop("disabled", false); // enable the submit button if valid phone number
                                else
-                                 $("#user_submit").prop("disabled", true); // disable the submit button if invalid phone number
+                                 $("#new_user[type='submit']").prop("disabled", true); // disable the submit button if invalid phone number
                              });
 
 
     });
 
+
+
 $(document).on('click', '#geolocateButton', function(){
-  $(this).prop("disabled", true)
-  $(this).html("Finding you...")
+  $("#geolocateButton").prop("disabled", true);
+  $("#geolocateButton").html("Locating you...");
 })
