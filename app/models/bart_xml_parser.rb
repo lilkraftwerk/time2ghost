cdc32757dae945fe6859c3b5bf2e9634b2585923
lilkraftwerk <lucasquinnmathis@gmail.com>
2014-04-15 15:11:13 -0700

    updated logic to find more than a single line for a given origin/destination

--- app/models/bart_xml_parser.rb
+++ app/models/bart_xml_parser.rb
@@ -1,17 +1,26 @@
 class BartXMLParser
-  def self.get_route_number_from_xml(route_xml)
-    Nokogiri::XML(route_xml).at_xpath('//leg').attributes["line"].value[-1]
+  def self.get_route_numbers_from_xml(route_xml)
+    # get all possible routes from origin to destination
+    routes = []
+    Nokogiri::XML(route_xml).xpath('//leg').each {|leg| routes << leg.attributes["line"].value[-1]}
+    routes.uniq!
   end
 
   def self.get_name_of_endpoint_station(endpoint_xml)
     Nokogiri::XML(endpoint_xml).at_xpath('//destination').content
   end
 
-  def self.filter_realtime_departures_by_correct_route(realtime_xml, endpoint)
+  def self.filter_realtime_departures_by_correct_route(realtime_xml, endpoints)
     path = Nokogiri::XML(realtime_xml).xpath('//etd')
-    correct_destination = path.xpath("//abbreviation[contains(text(), '#{endpoint}')]").first.parent()
-    departure_times = correct_destination.search('minutes').inject([]){ |all_times, departure_time| all_times << departure_time.text}
-    departure_times
+    possible_departures = {}
+    endpoints.each do |current_endpoint|
+      times_for_this_endpoint = path.xpath("//abbreviation[contains(text(), '#{current_endpoint}')]").first.parent()
+      possible_departures[current_endpoint] = []
+      times_for_this_endpoint.search('minutes').each{|x| possible_departures[current_endpoint] << x.text}
+    end
+    # returns hash with endpoint as key and realtime departures as minutes
+    # i.e {"endpoint" => ["1", "2", "3"]}
+      possible_departures
   end
 
   def self.parse_bart_stations_list(stations_xml)
