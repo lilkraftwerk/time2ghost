14d77db3efe188e362a644fe621721effe91b50b
Michael Gleason & Jordan Luyke <pair+fireflypulse.jluyke@devbootcamp.com>
2014-04-15 15:45:46 -0700

    Removed unused distance method. again.

--- app/models/station.rb
+++ app/models/station.rb
@@ -6,9 +6,4 @@ class Station < ActiveRecord::Base
     "#{self.latitude},#{self.longitude}"
   end
 
-  def distance_to(user_lat, user_long)
-    lat_difference = (user_lat - self.latitude.to_f).abs
-    long_difference = (user_long - self.longitude.to_f).abs
-    lat_difference + long_difference
-  end
 end
