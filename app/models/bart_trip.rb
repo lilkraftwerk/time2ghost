9b01100dc259ec3195414a22c1ab9e7512799c1e
Michael Gleason & Jordan Luyke <pair+fireflypulse.jluyke@devbootcamp.com>
2014-04-16 14:42:07 -0700

    Created valid? method for BartTrip model.

--- app/models/bart_trip.rb
+++ app/models/bart_trip.rb
@@ -13,6 +13,14 @@ class BartTrip < ActiveRecord::Base
     self.save!
   end
 
+  def valid?
+
+    return false unless self.current_location != nil
+    return false unless self.departure_station != self.destination_station
+
+    true
+  end
+
   def set_walking_time
     station_coordinates = Station.find_by_abbr(self.departure_station).gmap_destination_string
     self.walking_time = get_walking_time_to_station(current_location, station_coordinates)
